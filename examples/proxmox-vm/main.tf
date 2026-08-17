ad7cdc40e0e6a3e6831e9fb755147d9c73080cc9
Herick Motta <herickrossato@gmail.com>
2026-08-17 17:41:56 -0300

    Add reusable Proxmox cloud-init VM module.
    Give homelab-live a dummy-safe module to create Ubuntu guests without encoding live IPs in this repository.

---
+++ examples/proxmox-vm/main.tf
@@ -0,0 +1,19 @@
+module "example" {
+  source = "../../modules/proxmox-vm"
+
+  node_name       = "pve1"
+  vm_id           = 100
+  name            = "example-vm"
+  username        = "ubuntu"
+  ssh_public_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExampleKeyOnlyNotRealAAAAAAAAAAAA example@invalid"]
+  ipv4_address    = "192.0.2.10/24"
+  gateway         = "192.0.2.1"
+  dns_servers     = ["192.0.2.1"]
+  cloud_image_id  = "local:import/ubuntu-24.04-server-cloudimg-amd64.qcow2"
+  cores           = 1
+  memory_mb       = 2048
+  disk_gb         = 16
+  datastore_id    = "local-lvm"
+  bridge          = "vmbr0"
+  tags            = ["example"]
+}
